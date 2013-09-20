886544aaa552af8dd4c124efa98c94f273f0a6bb
David Chen <dchen@twitter.com>
2013-09-20 16:04:11 -0700

    update simulations

--- test.js
+++ test.js
@@ -9,40 +9,63 @@ var Utils = require("./lib/Utils");
 he.on("ready", ready)
 
 var TAG = "TEST";
+var LOOPS = 50000;
+
 
 function ready() {
   var ag = {
     hands: 0
   };
 
-  var loops = 10000;
 
-  var top = ["5s", "6h"];
+  var top = ["qc"];
+  var mid = ["kh"];
+  var bot = ["6s", "th", "3c"];
+  var draw = "5h";
 
-  for (var i = 0; i < loops; i++) {
-    var ev = testHand(top, "top");
-
-    ag.hands++;
-    ag[ev.handType] = ag[ev.handType] + 1 || 1;
-  }
-  
-  Utils.log(TAG, "High Card: ", ag[1], " ", ag[1]*100/ag.hands + "%");
-  Utils.log(TAG, "One Pair: ", ag[2], " ", ag[2]*100/ag.hands + "%");
+  testHands(top, mid, bot, draw);
 }
 
-function testHand(cards, position) {
+function testHands(top, mid, bot, draw) {
+  //run top
+  simulateHands(top.concat(draw), "top");
 
+  //run mid
+  simulateHands(mid.concat(draw), "middle");
+
+  //run bot
+  simulateHands(bot.concat(draw), "bottom");
+
+}
+
+function simulateHands(cards, position) {
+  Utils.log(TAG, "simulation", position);
+
+  var ag = {hands: 0, types: {}};
+
+  for (var i = 0; i < LOOPS; i++) {
+    var ev = simulateHand(cards, position);
+    ag.hands++;
+    ag.types[ev.handType] = ag.types[ev.handType] + 1 || 1;
+  }
+
+  for (handType in ag.types) {
+    var type = Constants.HAND_TYPES[handType];
+    var count = ag.types[handType];
+
+    Utils.log(TAG, "--", type, count * 100 / ag.hands + "%");
+
+  }
+}
+
+function simulateHand(cards, position) {
   var hand = new OFCHand(position, cards);
+
   //create deck without cards in hand
   var deck = Deck.without(cards);
   while(!hand.isFull()) {
     hand.add(deck.draw());
   }
-
   var ev = hand.evalHand();
-  //Utils.log(TAG, hand.cardNames());
-  //Utils.log(TAG, hand.evalHand());
-  // Utils.log(TAG, "royalties: ", hand.royalties());
-
   return ev;
 }
